8b188d8d7c5de1d47f2f4385409a1fbcd8232228
IraQi <mahmoud.must@hotmail.com>
2018-09-24 22:36:22 +0200

    final Update without Contact Form

--- src/app/pages/mainSide/Contact/contact.components.ts
+++ src/app/pages/mainSide/Contact/contact.components.ts
@@ -8,30 +8,30 @@ import { NgForm } from '../../../../../node_modules/@angular/forms';
 })
 export class ContactComponents implements OnInit {
 
-  @ViewChild('f')  contactForm: NgForm;
-  userData = {
-    userName : '',
-    userEmail: '',
-    userPhone: '',
-    userMajor: '',
-    userMessage: ''
-  };
+  // @ViewChild('f')  contactForm: NgForm;
+  // userData = {
+  //   userName : '',
+  //   userEmail: '',
+  //   userPhone: '',
+  //   userMajor: '',
+  //   userMessage: ''
+  // };
 
-  submitted= false;
+  // submitted= false;
 
   // onSubmit (form: NgForm){
   //   console.log(form);
   // }
   
 
-  onSubmit (){
-    this.submitted = true;
-    this.userData.userName = this.contactForm.value.Name;
-    this.userData.userEmail = this.contactForm.value.Mail;
-    this.userData.userPhone = this.contactForm.value.PhoneNum;
-    this.userData.userMajor = this.contactForm.value.majorSelect;
-    this.userData.userMessage = this.contactForm.value.Message;
-  }
+    // onSubmit (){
+    //   this.submitted = true;
+    //   this.userData.userName = this.contactForm.value.Name;
+    //   this.userData.userEmail = this.contactForm.value.Mail;
+    //   this.userData.userPhone = this.contactForm.value.PhoneNum;
+    //   this.userData.userMajor = this.contactForm.value.majorSelect;
+    //   this.userData.userMessage = this.contactForm.value.Message;
+    // }
 
   ngOnInit() {
   }
